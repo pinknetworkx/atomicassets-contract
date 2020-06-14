cddc188f0a3808ac5e84e8994066354bb2c5141d
Jona <jona.wilmsmann@tum.de>
2020-06-14 18:22:35 +0200

    Added max market fee constexpr to interface header

--- include/atomicassets-interface.hpp
+++ include/atomicassets-interface.hpp
@@ -15,6 +15,7 @@ using namespace eosio;
 using namespace std;
 
 namespace atomicassets {
+    static constexpr double MAX_MARKET_FEE = 0.15;
 
     static constexpr name ATOMICASSETS_ACCOUNT = name("atomicassets");
 
